bcaf75906b89218408d5972d7b06236771e68c8d
321999 <kishorejha321999@gmail.com>
2024-01-16 22:01:38 +0530

    perform all the arithmetic operation

---
+++ template.py
@@ -0,0 +1,8 @@
+# Taking first operand
+num1=int(input("Enter the number1"))
+
+# Taking operator from the user 
+operator=input("Enter the operation symbol u want to perform ")
+
+# Taking second operand
+num2=int(input("enter the number2"))
--- views.py
+++ views.py
@@ -1,4 +1,7 @@
 from model import *
+from template import *
 
 
-print(all_arithmetic_operations)
+if __name__=="__main__":
+    result=eval(f"{num1}{all_arithmetic_operations[operator]}{num2}")
+    print(f"{num1} {all_arithmetic_operations[operator]} {num2} = {result}")
